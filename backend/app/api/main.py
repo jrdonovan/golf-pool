fb7d9eb40a72b2c9aa96b12c36e875564d2a98e8
Joe Donovan <jrdonovan1004@gmail.com>
2026-03-31 19:12:32 -0600

    Organization routes

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter
 
-from app.api.routes import login, private, users, utils
+from app.api.routes import login, organizations, private, users, utils
 from app.core.config import settings
 
 api_router = APIRouter()
 api_router.include_router(login.router)
+api_router.include_router(organizations.router)
 api_router.include_router(users.router)
 api_router.include_router(utils.router)
 
